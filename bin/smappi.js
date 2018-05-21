7eb5789dd8cf62ba7433f0cdf9133d5bd1050a9f
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2018-05-21 13:29:43 +0300

    Smappi doctap implemented (mask ... added for object and array)

--- bin/smappi.js
+++ bin/smappi.js
@@ -6,32 +6,32 @@ var yargs = require('yargs');
 var commands = require('../lib/commands');
 
 var argv = yargs
-  .strict()
-  .command(commands.run)
-  .fail(function(msg, error) {
-    if (error) {
-      throw error;
-    } else {
-      yargs.showHelp('error');
-      console.error(msg);
-      return yargs.exit(1);
-    }
-  })
-  .example('smappi run')
-  .version()
-  .usage(
-    `Usage:
+    .strict()
+    .command(commands.run)
+    .command(commands.test)
+    .fail(function(msg, error) {
+        if (error) {
+            throw error;
+        } else {
+            yargs.showHelp('error');
+            console.error(msg);
+            return yargs.exit(1);
+        }
+    })
+    .example('smappi run')
+    .example('smappi test')
+    .version()
+    .usage(`Usage:
 
-  # Run api.js as Simple Http Server
+  # Run api.js as Http Dev Server
   $0 run localhost:8000
 
   # Run doctests
-  npm test`
-  )
-  .recommendCommands()
-  .help().argv;
+  $0 test`)
+    .recommendCommands()
+    .help().argv;
 
 if (!argv._handled) {
-  yargs.showHelp('error');
-  process.exit(1);
+    yargs.showHelp('error');
+    process.exit(1);
 }
